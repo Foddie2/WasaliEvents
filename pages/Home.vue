d1397e0c4eec081827ca400bce119ef8e561cda9
Foddie2 <Bensonagala@live.com>
2022-07-28 13:47:05 +0300

    debugging

---
+++ pages/Home.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <section class="vh-100" style="background-color: #FCFAFA;">
+
+
+    </section>
+
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <Tutorial />
+
+<section class="vh-100" style="background-color: #FEFBF6;">
+  <!-- <div class="container py-5 h-100"> -->
+    <Header/>
+
+
+
+
+
+</section>
 </template>
 
 <script>
+import Header from "../components/Header.vue";
 export default {
-  name: 'IndexPage',
+    components: { Header }
 }
 </script>
